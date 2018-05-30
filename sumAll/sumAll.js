9daff344cd2943d2bbc551ba285714728245c7fd
Chad Kreutzer <kanthalion@gmail.com>
2018-05-30 12:57:50 +0000

    improved sum all

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,4 @@
-var sumAll = function(num1, num2) {
+const sumAll = function(num1, num2) {
     const args = [...arguments].sort();
     return (args.every(num => Number.isInteger(num) && num >= 0))
         ? Array.from(Array(args[1] - args[0] + 1), (_,x) => x + args[0]).reduce((a,b) => a + b)
