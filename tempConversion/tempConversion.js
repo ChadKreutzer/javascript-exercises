0c75446e9b0fa32dfc19929e1bde34c6fa87c6dd
Chad Kreutzer <kanthalion@gmail.com>
2018-05-31 08:56:24 +0000

    replace var with const in temp conversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,6 +1,6 @@
-var ftoc = f => Math.round(50 / 9 * f - 1600 / 9) / 10;
+const ftoc = f => Math.round(50 / 9 * f - 1600 / 9) / 10;
 
-var ctof = c => Math.round(18 * c + 320) / 10;
+const ctof = c => Math.round(18 * c + 320) / 10;
 
 module.exports = {
   ftoc,
