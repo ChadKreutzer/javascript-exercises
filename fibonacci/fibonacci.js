050ab14a8f47685bd7efef5cfc6baf53b8eda01a
Chad Kreutzer <kanthalion@gmail.com>
2018-05-31 11:50:38 +0000

    solved palindrome

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-var fibonacci = function() {
+var fibonacci = function(n) {
+    if(n < 0) return "OOPS";
+    if (n === 0) return 0;
+    return (n === 1) ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
+};
 
-}
-
-module.exports = fibonacci
+module.exports = fibonacci;
--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,9 @@
-var palindromes = function() {
+const palindromes = function(str) {
+    const test = str
+        .toLowerCase()
+        .split('')
+        .filter(x => /([a-zA-Z])/.test(x));
+    return test.join('') === test.reverse().join('');
+};
 
-}
-
-module.exports = palindromes
+module.exports = palindromes;
