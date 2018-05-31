2db437b764c58ae7f4496c4f78d0646493fbeb55
Chad Kreutzer <kanthalion@gmail.com>
2018-05-31 08:32:58 +0000

    solved leap year

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,7 @@
-var leapYears = function() {
+const leapYears = function(year) {
+    const isLeap400 = year % 400 === 0;
+    const isLeapOther = year % 4 === 0 && year % 100;
+    return (isLeap400 || isLeapOther) ? true : false;
+};
 
-}
-
-module.exports = leapYears
+module.exports = leapYears;
