94b11bf8a9776d0156a5735eed693bcc7f034db7
Chad Kreutzer <kanthalion@gmail.com>
2018-06-01 14:42:24 +0000

    snake_case

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,6 @@
-var snakeCase = function() {
+const snakeCase = function(str){
+    const prepped = (str.match(/[^\w]/) === null) ? str.replace(/([A-Z])/g, " $&") : str;
+    return prepped.match(/\b\w+/g).join('_').toLowerCase();
+};
 
-}
-
-module.exports = snakeCase
+module.exports = snakeCase;
