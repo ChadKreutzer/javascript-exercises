d50abb4dfffba8419a4f6d0441e020bb8886ce09
Chad Kreutzer <kanthalion@gmail.com>
2018-06-01 11:16:23 +0000

    solved pig latin

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,9 +1,14 @@
-function translate() {
-	// body...
+const replacer = (match, p1, p2, p3, offset, string) => (p1) ? `${p2}${p1}ay` : `${p3}ay`;
+
+function translate(str) {
+	const pigizer = /(^\w?qu|^[^aeiouy]+)(\w+)|(\w+)/;
+	return str
+		.split(' ')
+		.map(word => word.replace(pigizer, replacer))
+		.join(' ');
 }
 
 
 module.exports = {
 	translate
-}
-
+};
